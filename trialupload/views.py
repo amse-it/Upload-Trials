e8a77f099fac5fd5d6258fbc7ef667f3edf5962b
amse-it <rougeluna_95@yahoo.com>
2015-06-25 13:23:54 +0800

    updated views.py: added index view

--- trialupload/views.py
+++ trialupload/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from .forms import UploadFileForm
+
+# Imaginary function to handle an uploaded file.
+from .addfunc import handle_uploaded_file
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render_to_response('upload.html', {'form': form})
+
